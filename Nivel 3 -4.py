9ecb18cf301d124869f15f23ce2c18fff605c7ce
mjimenez157 <mjimenez157@unab.edu.co>
2026-03-07 17:14:11 -0500

    Nivel 3 -4.py

--- Nivel 3 -4.py
+++ Nivel 3 -4.py
@@ -1,1 +1,7 @@
 def buscar_palabra(cadena, palabra):
+  if palabra in cadena:
+    return True
+  else:
+    return False
+print(buscar_palabra("hola mundo", "mundo"))
+print(buscar_palabra("hola mundo", "casa"))
