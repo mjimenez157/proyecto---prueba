fa7c93ed6e5aaf4113ac0a00907d009dee6549d5
mjimenez157 <mjimenez157@unab.edu.co>
2026-03-07 15:57:00 -0500

    Nivel 1 -4.py

--- Nivel 1 -4.py
+++ Nivel 1 -4.py
@@ -1,12 +1,10 @@
-class Vehiculo:
-    marca = "honda"
-    modelo = "20"
-    color = "negro"
-    
-    def arrancar(self):
-        print(f"el {sef.marca}, {sef.modelo}, {sef.color} arranca")
-        
-        
-moto = Vehiculo()
-print(moto.marca, moto.modelo)
-moto.arrancar()
+def contador_vocales():
+    cadena = input("Ingrese una frase: ")
+    vocales = "aeiou"
+    contador =0
+    for letra in cadena:
+        if letra in vocales:
+            contador +=1
+    return contador
+resultado = contador_vocales()
+print("Número de vocales: ", resultado)
