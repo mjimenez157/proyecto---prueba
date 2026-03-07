8b81c65d6e6a8ed00d2a2e38fc48c4f2a1ccf855
mjimenez157 <mjimenez157@unab.edu.co>
2026-03-07 18:11:43 -0500

    Nivel 4 -4.py

--- Nivel 4 -4.py
+++ Nivel 4 -4.py
@@ -1,1 +1,10 @@
-def
+def calcular_mediana(*numeros):
+  lista = sorted(numeros)
+  num = len(lista)
+  mitad = num // 2
+  if num % 2 != 0: # impar
+    return lista[mitad]
+  else: # par
+     return (lista[mitad - 1] + lista [mitad]) / 2
+print(calcular_mediana(9, 1, 5))
+print(calcular_mediana(9, 1, 5, 3))
