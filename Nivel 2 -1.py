996315a968fba332279d1a34aeb8fc79ad1fa9e1
mjimenez157 <mjimenez157@unab.edu.co>
2026-03-07 16:16:50 -0500

    Nivel 2 -1.py

--- Nivel 2 -1.py
+++ Nivel 2 -1.py
@@ -1,12 +1,7 @@
-class Persona:
-    nombre = "juan"
-    edad = "18"
-    
-    
-    def estudiar(self):
-        self.estudiar = True
-        return "persona que estudia..."
-
-estudiante = Persona()
-print(estudiante.nombre)
-print(estudiante.estudiar())
+def palindromo(palabra):
+    if palabra == palabra [::-1]:
+        return True
+    else:
+        return False
+print(palindromo("ana")
+print(palindromo("hola"))
