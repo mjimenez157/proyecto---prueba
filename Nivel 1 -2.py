ccc09cf464397e710585a1c866da9342e4753575
mjimenez157 <mjimenez157@unab.edu.co>
2026-03-07 14:37:52 -0500

    Nivel 1 -2.py

--- Nivel 1 -2.py
+++ Nivel 1 -2.py
@@ -1,1 +1,6 @@
-def suma
+def suma():
+  num1 = int(input("Ingrese un número: "))
+  num2 = int(input("Ingrese otro número: "))
+  resultado_suma = num1 + num2
+  print("La suma es: ", resultado_suma)
+suma()
