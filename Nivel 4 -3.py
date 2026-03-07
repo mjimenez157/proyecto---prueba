4f9ea2b1163e8418d711c3948fac76a2263df543
mjimenez157 <mjimenez157@unab.edu.co>
2026-03-07 17:48:26 -0500

    Nivel 4 -3.py

--- Nivel 4 -3.py
+++ Nivel 4 -3.py
@@ -1,1 +1,7 @@
-def 
+def mayor_de_varios_numeros(*numeros):
+  mayor = numeros[0]
+  for num in numeros:
+    if num > mayor:
+      mayor = num
+  return mayor
+print(mayor_de_varios_numeros(7, 13, 15, 9, 5))
